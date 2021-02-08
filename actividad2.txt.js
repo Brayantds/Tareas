27b2ac707a64874f7a47db2b85282c3e60c09e89
Brayan Martinez <brianmartinez18bamg@gmail.com>
2021-02-08 10:17:21 -0600

    segundo cambio

---
+++ actividad2.txt.js
@@ -0,0 +1,14 @@
+var mes = 10
+var mes = prompt("Introdusca el mes")
+if(mes >= 1 && mes <= 2 ){ 
+  alert("es invierno")
+}else if(mes >= 3 && mes <= 5){
+  alert("es primavera")
+}else if(mes >= 6 && mes <= 8){
+  alert("es verano")
+}else if(mes >= 9 && mes <= 11){
+  alert("es otoño")
+} else if(mes == 12){
+  alert("es invierno")
+}
+  alert("16003497 brayan Alexander Martinez Garcia")
